17cf96d1b63fd9df482fb09c62dfb9838aaebb0f
Michael Standen <michael@aocollab.tech>
2025-04-08 09:11:49 +1200

    Remove eslint from vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,10 @@
 import path from 'path'
 import {defineConfig} from 'vite'
-import eslint from 'vite-plugin-eslint'
 
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    eslint({
-      include: ['src/**/*.ts', 'src/**/*.tsx'],
-    }),
-  ],
+  plugins: [react()],
   base: '/',
   publicDir: 'public',
   build: {
